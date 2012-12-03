6bf35a2d0b0853d6b0aedb6078a02349419180fa
lecaros <jose.lecaros@excelsys.net>
2012-12-03 18:39:41 -0300

    agrega interacción con usuario
    Signed-off-by: lecaros <jose.lecaros@excelsys.net>

--- src/main/java/com/merkenlabs/ucinf/seguridad/EncryptDecryptMain.java
+++ src/main/java/com/merkenlabs/ucinf/seguridad/EncryptDecryptMain.java
@@ -1,5 +1,7 @@
 package com.merkenlabs.ucinf.seguridad;
 
+import javax.swing.JOptionPane;
+
 /**
  * @author lecaros
  * 
@@ -7,6 +9,16 @@ package com.merkenlabs.ucinf.seguridad;
 public class EncryptDecryptMain {
 
 	public static void main(String[] args) {
+		String key = JOptionPane.showInputDialog(null,
+				"Indique el caracter llave:");
 
+		String string = JOptionPane.showInputDialog(null, "Indique la cadena");
+
+		EncryptDecrypt encrypt = new EncryptDecrypt();
+		String result = encrypt.Encrypt(string, key.charAt(0));
+
+		JOptionPane.showMessageDialog(null, "Llave : " + key + "\n"
+				+ "Cadena inicial:" + string + "\n" + "Cadena final  :"
+				+ result);
 	}
 }
